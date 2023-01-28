f3da65cd1a89b92c40e5b7c9bfa34df3c37ca3fa
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-01-28 07:33:40 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int Num = int.Parse(Console.ReadLine());
+Console.WriteLine("Число четное?");
+
+if (Num % 2 == 0) Console.WriteLine("Да.");
+else Console.WriteLine("Нет.");
