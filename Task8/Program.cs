3f42c27a356f240e823ce1a4caf53ed7fff40146
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-01-28 07:49:51 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int Num = int.Parse(Console.ReadLine());
+int Digit = 2;
+
+if (Num == 1) Console.Write("Четных чисел нет.");
+if (Num > 1)
+{
+    while (Digit <= Num)
+    {
+        Console.Write(Digit + " ");
+        Digit += 2;
+    }
+}
+if (Num < 1) Console.Write("Число меньше 1.");
