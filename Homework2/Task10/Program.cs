369f7c7c37cf2da3c4a663f4f851378d5e02ac68
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-01-31 19:15:44 +0300

    Task10

---
+++ Homework2/Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int Num = int.Parse(Console.ReadLine());
+if (Num / 100 == 0) Console.WriteLine("Число не трехзначное.");
+else
+{
+    Console.WriteLine(Num / 10 % 10);
+}
