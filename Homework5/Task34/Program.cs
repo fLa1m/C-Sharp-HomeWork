250ed264b8bce452a75531a08a4b1eb0c7d30ca1
Andrey Mokin <parkovka.mokin@yandex.ru>
2023-02-12 10:43:57 +0300

    Homework 5 autosave on

--- Homework5/Task34/Program.cs
+++ Homework5/Task34/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
--- Homework5/Task36/Program.cs
+++ Homework5/Task36/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
