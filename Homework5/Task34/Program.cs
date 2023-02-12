0174fa0e5ea72cc8a1dc63588583af06e499a381
Andrey Mokin <parkovka.mokin@yandex.ru>
2023-02-12 11:07:47 +0300

    Task34

--- Homework5/Task34/Program.cs
+++ Homework5/Task34/Program.cs
@@ -1,2 +1,51 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int DataEntry(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+int EvenNum(int[] array)
+{
+    int count = 0;
+    foreach (int i in array)
+    {
+        if (i % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+int length = DataEntry("Введите длину массива: ");
+int min = DataEntry("Введите минимальное значение интервала: ");
+int max = DataEntry("Введите максимальное значение интервала: ");
+int[] array = GenerateArray(length, min, max);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine($"Количество четных чисел в массиве: {EvenNum(array)}");
