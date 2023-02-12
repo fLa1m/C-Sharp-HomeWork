02a9a9dfb80374b1753f976538e0f6bec36c0183
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-12 12:10:24 +0300

    Task36 start

--- Homework5/Task36/Program.cs
+++ Homework5/Task36/Program.cs
@@ -1,2 +1,42 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
+
+int DataEntry(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+int SumOfUnevenElements(int[] array)
+{
+
+}
+
+Console.Clear();
+int length = DataEntry("Введите длину массива: ");
+int min = DataEntry("Введите минимальное значение интервала: ");
+int max = DataEntry("Введите максимальное значение интервала: ");
+int[] array = GenerateArray(length, min, max);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
