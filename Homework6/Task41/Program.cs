a1e4ffb2caaa09aaf80ea7101cc4dbbc8d2e6dc6
Andrey Mokin <parkovka.mokin@yandex.ru>
2023-02-15 21:45:09 +0300

    Homework 6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string[] InputArray(string message)
+{
+    Console.Write(message);
+    string[] str = Console.ReadLine().Split(" "); // добавление в массив по разделителю
+    return str;
+}
+
+int[] ChangeType(string[] array)
+{
+    int[] numArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        numArray[i] = int.Parse(array[i]);
+    }
+    return numArray;
+}
+
+Console.Clear();
+string[] inputNum = InputArray("Введите числа через пробел: ");
+int[] array = ChangeType(inputNum);
+int count = 0;
+for (int i = 0; i < inputNum.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел > 0: {count}");
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, 
+//  y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// y = ax + c; y = bx + d; (d - c/a - b; ad - bc/a - b)
+
+double InputNum(string message)
+{
+    Console.Write(message);
+    double num = double.Parse(Console.ReadLine());
+    return num;
+}
+
+string CrossPoint(double a, double b, double c, double d)
+{
+    double pointOne = (d - c) / (a - b);
+    double pointTwo = (a * d - b * c) / (a - b);
+    string point = "(" + pointOne + "; " + pointTwo + ")";
+    return point;
+}
+
+Console.Clear();
+double k1 = InputNum("Задайте значение k1: ");
+double b1 = InputNum("Задайте значение b1: ");
+double k2 = InputNum("Задайте значение k2: ");
+double b2 = InputNum("Задайте значение b2: ");
+Console.WriteLine($"Точка пересечения прямых: {CrossPoint(k1, k2, b1, b2)}");
