119dc40cff125a22bdeb9852bdec16e0fd7cb62a
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-16 19:42:02 +0300

    add etalon

---
+++ Homework6/Task41_Top/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Считать число с консоли
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value); // преобразует строку в целое число
+    return result;
+}
+
+// Ввести массив
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+int length = Prompt("Введите количество элементов: ");
+int[] array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0: {CountPositiveNumbers(array)}");
