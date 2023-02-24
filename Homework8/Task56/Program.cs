7424d313a1376d918e11bc45559a4c17d7ea5e57
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-24 08:47:24 +0300

    Homework 8 end

--- Homework8/Task56/Program.cs
+++ Homework8/Task56/Program.cs
@@ -1,2 +1,69 @@
 ﻿//  Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить 
 //  строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int DataInput(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumOfRow(int[,] array)
+{
+    int minSum = 0;
+    int rowSum = 0;
+    int minRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minSum += array[0, i];
+    }
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSum += array[i, j];
+        }
+        if (rowSum < minSum) { minSum = rowSum; minRow = i; }
+        rowSum = 0;
+    }
+    Console.WriteLine($"Минимальная сумма в {minRow + 1} строке.");
+}
+
+Console.Clear();
+int rows = DataInput("Введите количество рядов: ");
+int columns = DataInput("Введите количество столбцов: ");
+int[,] matrix = FillArray(rows, columns);
+Console.WriteLine();
+PrintArray(matrix);
+SumOfRow(matrix);
--- Homework8/Task58/Program.cs
+++ Homework8/Task58/Program.cs
@@ -1,1 +1,77 @@
 ﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int DataInput(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MatrixMultiplication(int[,] arrayA, int[,] arrayB)
+{
+    int[,] multMatrix = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    int mult = 0;
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(0); k++)
+            {
+                mult = arrayA[i, k] * arrayB[k, j];
+                multMatrix[i, j] += mult;
+            }
+            mult = 0;
+        }
+    }
+    return multMatrix;
+}
+
+int a, b, c, d;
+a = DataInput("Введите количество строк матрицы А: ");
+do
+{
+    b = DataInput("Введите количество столбцов матрицы А: ");
+    c = DataInput("Введите количество строк матрицы B: ");
+    if (b != c) { Console.WriteLine("Количестов столбцов матрицы A должно быть равно количеству строк матрицы B."); }
+} while (b != c);
+d = DataInput("Введите количество столбцов матрицы B: ");
+
+int[,] matrixA = FillArray(a, b);
+int[,] matrixB = FillArray(c, d);
+PrintArray(matrixA);
+Console.WriteLine();
+PrintArray(matrixB);
+int[,] matrixAB = MatrixMultiplication(matrixA, matrixB);
+Console.WriteLine();
+PrintArray(matrixAB);
