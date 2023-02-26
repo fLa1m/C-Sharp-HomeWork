a4a63063f6abd9c66f1dfc6df42620cb83ecd7f0
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-26 12:35:34 +0300

    Homework 9 complited

--- Homework9/Task66/Program.cs
+++ Homework9/Task66/Program.cs
@@ -2,3 +2,25 @@
 // в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int SumNumber(int a, int b)
+{
+    if (a > b) { return 0; }
+    else { return a + SumNumber(a + 1, b); }
+}
+
+int DataInput(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int m, n;
+do
+{
+    m = DataInput("Введите начало интервала: ");
+    n = DataInput("Введите конец интервала: ");
+    if (m > n) { Console.WriteLine("Начальное значение интервала должно быть больше конечного."); }
+} while (m > n);
+Console.WriteLine(SumNumber(m, n));
